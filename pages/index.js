99d4c4818026a54d2fb69346ce92cfc20a7841ab
unknown <sarahaghiri88@gmail.com>
2023-03-11 13:20:01 +0330

    create input

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,22 @@ const index = () => {
           </div>
         </div>
 
-        
+        <div className="h-[90%] flex flex-col  ">
+        <div className="h-[50%] w-full  flex items-center justify-center pt-16">
+          <p className="text-gray-300 text-4xl">Welcome to io-team-website</p>
+        </div>
+        <div className=" flex  justify-center w-full">
+          <input
+            placeholder="search by Name,Projects,etc"
+            className="bg-gray-300 w-[30%] text-xs  px-5 rounded-xl mx-2"
+          ></input>
+          <button className=" text-white rounded-xl py-3 px-12 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            LOOK UP
+          </button>
+        </div>
+      </div>
+
+
       </div>
     </section>
   );
