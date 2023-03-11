203a0d935811675fdbfcfb67f162ca9ef2cebb26
unknown <sarahaghiri88@gmail.com>
2023-03-11 13:11:34 +0330

    add header

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,29 @@ const index = () => {
   return (
     <section>
       <div className="h-screen w-full background ">
-       
+        <div className="h-[10%]">
+          <div className="flex items-center justify-between p-5">
+            <div>
+              <p className="text-white">IO TEAM</p>
+            </div>
+            <div className="flex items-center gap-x-10">
+              <div>
+                <p className="text-white text-sm">SIGN IN</p>
+              </div>
+              <div>
+                {' '}
+                <button className="border border-orange-300 px-3 py-1 rounded-md text-white text-sm">
+                  LOGIN
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
 
+        
       </div>
     </section>
   );
-}
+};
 
 export default index;
